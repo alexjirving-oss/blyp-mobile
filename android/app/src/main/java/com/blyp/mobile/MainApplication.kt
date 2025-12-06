aed59869bdeb5ad13115486d79bccc4841089ca6
Alex Irving <alex@blyp.com>
2025-12-06 11:26:16 +0000

    fix: add missing IVSPackage import to MainApplication

--- android/app/src/main/java/com/blyp/mobile/MainApplication.kt
+++ android/app/src/main/java/com/blyp/mobile/MainApplication.kt
@@ -16,6 +16,8 @@ import com.facebook.react.defaults.DefaultReactNativeHost
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
 
+import com.blyp.mobile.ivs.IVSPackage
+
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
